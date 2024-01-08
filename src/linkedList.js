9eedd42773756bf3c3dead18e7463ba41368a05b
IkkyPT <ricardoo1998carvalho@gmail.com>
2024-01-08 13:20:46 +0000

    Structured a linked list module with the following functionalities:
    1. **createLinkedList**: Initializes a new linked list with head and tail set to null.
    
    2. **prepend**: Adds a new node containing a given value to the start of the list. Updates head and tail accordingly.
    
    3. **append**: Adds a new node containing a given value to the end of the list. Adjusts head and tail as needed.
    
    4. **size**: Computes and returns the total number of nodes in the list.
    
    5. **getHead**: Retrieves and returns the first node in the list.
    
    6. **getTail**: Retrieves and returns the last node in the list.
    
    7. **at**: Allows access to a node at a specific index in the list. Returns null if the index is out of bounds.

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,1 +1,94 @@
-console.log("9");
+import createNode from "./node";
+
+// Create Linked List function
+const linkedList = (() => {
+    function createLinkedList() {
+        return {
+            head: null,
+            tail: null,
+            append,
+            prepend,
+            size,
+            getHead,
+            getTail,
+            at,
+        };
+    }
+
+    // adds a new node containing value to the start of the list
+    function prepend(value) {
+        const newNode = createNode(value, this.head);
+        this.head = newNode;
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+    }
+
+    // adds a new node containing value to the end of the list
+    function append(value) {
+        const newNode = createNode(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.nextNode = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    // returns the total number of nodes in the list
+    function size() {
+        let count = 0;
+        let current = this.head;
+        while (current) {
+            current = current.nextNode;
+            count++;
+        }
+        return count;
+    }
+
+    // returns the first node in the list
+    function getHead() {
+        const headNode = this.head;
+        return headNode;
+    }
+
+    // returns the last node in the list
+    function getTail() {
+        const tailNode = this.tail;
+        return tailNode;
+    }
+
+    function at(index) {
+        let nodeIndex = 0;
+        let current = this.head;
+        while(current) {
+            if(index === nodeIndex) {
+                return current;
+            }
+            current = current.nextNode;
+            nodeIndex++;
+        }
+        return null;
+    }
+
+
+
+    return {
+        createLinkedList,
+        append,
+        prepend,
+        size,
+        getHead,
+        getTail,
+        at,    
+    };
+})();
+
+console.log(linkedList.prepend(69));
+console.log(linkedList.append(68));
+console.log(linkedList.size());
+console.log(linkedList.getHead());
+console.log(linkedList.getTail());
+console.log(linkedList.at(0));
